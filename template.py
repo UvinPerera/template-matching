75706ed71e6efc9e5b8d81d9ad2d5d01455027fd
UvinPerera <uvininduwaraperera@gmail.com>
2021-08-09 21:08:39 +0530

    new comments

--- template.py
+++ template.py
@@ -12,8 +12,8 @@ loc = np.where( result >= threshold)
 
 num_of_occurences =0 # number of occurences
 for pt in zip(*loc[::-1]):
-    cv.rectangle(image, pt, (pt[0] + width, pt[1] + height), (0,255,0), 2)
-    num_of_occurences=num_of_occurences+1
+    cv.rectangle(image, pt, (pt[0] + width, pt[1] + height), (0,255,0), 2) # draw rectangles in the indentified regions
+    num_of_occurences=num_of_occurences+1 # increment the number ofoccurences
     
-cv.imwrite('result.png',image)
-print("Number of Occurences:"+str(num_of_occurences))
+cv.imwrite('result.png',image) # Save the image after highliting the regions
+print("Number of Occurences:"+str(num_of_occurences)) # print the number of occurences
